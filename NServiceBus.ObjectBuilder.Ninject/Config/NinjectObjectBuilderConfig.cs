8e56fbd7bf4ca1f1815d43575236b0bf23831d3a
Aaron Jensen <aaronjensen@gmail.com>
2009-11-30 23:36:15 -0800

    initial commit

---
+++ NServiceBus.ObjectBuilder.Ninject/Config/NinjectObjectBuilderConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Ninject;
+using NServiceBus.ObjectBuilder.Common.Config;
+
+namespace NServiceBus.ObjectBuilder.Ninject.Config
+{
+	public static class NinjectObjectBuilderConfig
+	{
+		public static Configure NinjectBuilder(this Configure config, IKernel kernel)
+		{
+			ConfigureCommon.With(config, new NinjectObjectBuilder(kernel));
+			return config;
+		}
+	}
+}
---
+++ NServiceBus.ObjectBuilder.Ninject/NinjectObjectBuilder.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Ninject;
+using Ninject.Parameters;
+using NServiceBus.ObjectBuilder.Common;
+
+namespace NServiceBus.ObjectBuilder.Ninject
+{
+	public class NinjectObjectBuilder : IContainer
+	{
+		readonly IKernel _kernel;
+		readonly IDictionary<Type, IList<IParameter>> _parameters = new Dictionary<Type, IList<IParameter>>();
+
+		public NinjectObjectBuilder(IKernel kernel)
+		{
+			_kernel = kernel;
+		}
+
+		public object Build(Type typeToBuild)
+		{
+			if (_parameters.ContainsKey(typeToBuild))
+			{
+				return _kernel.Get(typeToBuild, _parameters[typeToBuild].ToArray());
+			}
+
+			return _kernel.Get(typeToBuild);
+		}
+
+		public IEnumerable<object> BuildAll(Type typeToBuild)
+		{
+			return _kernel.GetAll(typeToBuild);
+		}
+
+		public void Configure(Type component, ComponentCallModelEnum callModel)
+		{
+			if (callModel == ComponentCallModelEnum.Singleton)
+			{
+				_kernel.Bind(component).ToSelf().InSingletonScope();
+			} 
+			else if (callModel == ComponentCallModelEnum.Singlecall)
+			{
+				_kernel.Bind(component).ToSelf().InTransientScope();
+			}
+			else throw new ArgumentException("callModel");
+		}
+
+		public void ConfigureProperty(Type component, string property, object value)
+		{
+			if (!_parameters.ContainsKey(component))
+			{
+				_parameters[component] = new List<IParameter>();
+			}
+
+			_parameters[component].Add(new PropertyValue(property, value));
+		}
+
+		public void RegisterSingleton(Type lookupType, object instance)
+		{
+			_kernel.Bind(lookupType).ToConstant(instance);
+		}
+	}
+}
