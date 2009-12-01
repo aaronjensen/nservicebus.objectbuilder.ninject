087d2de818458fb53d048a3688634a8cd816e4d8
Aaron Jensen <aaronjensen@gmail.com>
2009-12-01 07:07:28 -0800

    Doing things differently, using a more aggressive ninject

--- NServiceBus.ObjectBuilder.Ninject/NinjectObjectBuilder.cs
+++ NServiceBus.ObjectBuilder.Ninject/NinjectObjectBuilder.cs
@@ -2,10 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using Ninject;
 using Ninject.Parameters;
+using Ninject.Planning.Strategies;
+using Ninject.Selection;
+using Ninject.Selection.Heuristics;
+using Ninject.Syntax;
 using NServiceBus.ObjectBuilder.Common;
 
 namespace NServiceBus.ObjectBuilder.Ninject
@@ -13,17 +18,19 @@ namespace NServiceBus.ObjectBuilder.Ninject
 	public class NinjectObjectBuilder : IContainer
 	{
 		readonly IKernel _kernel;
-		readonly IDictionary<Type, IList<IParameter>> _parameters = new Dictionary<Type, IList<IParameter>>();
+	  readonly ObjectBuilderPropertyHeuristic _heuristic;
 
 		public NinjectObjectBuilder(IKernel kernel)
 		{
 			_kernel = kernel;
+
+		  _heuristic = new ObjectBuilderPropertyHeuristic();
+		  _kernel.Components.Get<ISelector>().InjectionHeuristics.Add(_heuristic);
 		}
 
 		public object Build(Type typeToBuild)
 		{
-		  var parameters = _parameters.Where(x => typeToBuild.IsAssignableFrom(x.Key)).SelectMany(x => x.Value).ToArray();
-		  var output = _kernel.Get(typeToBuild, parameters);
+		  var output = _kernel.Get(typeToBuild);
 
       return output;
 		}
@@ -44,16 +51,19 @@ namespace NServiceBus.ObjectBuilder.Ninject
 				_kernel.Bind(component).ToSelf().InTransientScope();
 			}
 			else throw new ArgumentException("callModel");
+
+		  _heuristic.RegisteredTypes.Add(component);
 		}
 
 		public void ConfigureProperty(Type component, string property, object value)
 		{
-			if (!_parameters.ContainsKey(component))
-			{
-				_parameters[component] = new List<IParameter>();
-			}
+		  var bindings = _kernel.GetBindings(component);
+      if (!bindings.Any()) throw new ArgumentException("Component not registered: " + component);
 
-			_parameters[component].Add(new PropertyValue(property, value));
+      foreach (var binding in bindings)
+      {
+        binding.Parameters.Add(new PropertyValue(property, value));
+      }
 		}
 
 		public void RegisterSingleton(Type lookupType, object instance)
@@ -61,4 +71,34 @@ namespace NServiceBus.ObjectBuilder.Ninject
 			_kernel.Bind(lookupType).ToConstant(instance);
 		}
 	}
+
+  class ObjectBuilderPropertyHeuristic : IInjectionHeuristic
+  {
+    public IList<Type> RegisteredTypes { get; private set; }
+
+    public ObjectBuilderPropertyHeuristic()
+    {
+      RegisteredTypes = new List<Type>();
+    }
+
+    public void Dispose()
+    {
+    }
+
+    public INinjectSettings Settings
+    {
+      get; set;
+    }
+
+    public bool ShouldInject(MemberInfo member)
+    {
+      var propertyInfo = member as PropertyInfo;
+      if (propertyInfo == null) return false;
+
+      if (RegisteredTypes.Where(x => propertyInfo.DeclaringType.IsAssignableFrom(x)).Any() && RegisteredTypes.Where(x => propertyInfo.PropertyType.IsAssignableFrom(x)).Any())
+        return true;
+
+      return false;
+    }
+  }
 }
